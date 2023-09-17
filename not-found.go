2697ecfd61a8e00e64aee5ba5a3b1e26fc761f41
Leonardo Gnutzmann <leognutzmann@gmail.com>
2023-09-17 01:38:39 -0300

    refactor: create struct to not found response

--- not-found.go
+++ not-found.go
@@ -4,20 +4,22 @@ import (
 	"github.com/kataras/iris/v12"
 )
 
+type notFoundResponse struct {
+	Message string   `json:"message"`
+	Routes  []string `json:"routes,omitempty"`
+}
+
 func notFound(ctx iris.Context) {
 	suggestPaths := ctx.FindClosest(3)
 
 	if len(suggestPaths) == 0 {
-		ctx.JSON(struct {
-			Message string `json:"message"`
-		}{Message: "Route not found"})
+		ctx.JSON(notFoundResponse{
+			Message: "Route not found",
+		})
 		return
 	}
 
-	ctx.JSON(struct {
-		Message string   `json:"message"`
-		Routes  []string `json:"routes"`
-	}{
+	ctx.JSON(notFoundResponse{
 		Message: "Route not found. Check these similar routes and try again",
 		Routes:  suggestPaths,
 	})
